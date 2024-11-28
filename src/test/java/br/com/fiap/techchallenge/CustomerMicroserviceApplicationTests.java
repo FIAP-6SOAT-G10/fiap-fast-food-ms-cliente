bc587d8b63894784be542b36f94c373a62308657
sourceGabriel <fontegabriel@gmail.com>
2024-11-28 00:40:44 -0300

    Test test

--- src/test/java/br/com/fiap/techchallenge/CustomerMicroserviceApplicationTests.java
+++ src/test/java/br/com/fiap/techchallenge/CustomerMicroserviceApplicationTests.java
@@ -2,13 +2,8 @@ package br.com.fiap.techchallenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "logging.level.org.springframework=DEBUG",
-    "logging.level.br.com.fiap.techchallenge=DEBUG"
-})
 class CustomerMicroserviceApplicationTests {
 
     @Test
